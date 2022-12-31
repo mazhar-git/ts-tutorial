ebf01e4a2586c3b23a6fae63a03fb289310a217c
mazhar_laptop <mazhar@MAZHAR-PC>
2022-12-31 16:51:57 +0530

    Inheritance in Typescript

---
+++ src/Animal.ts
@@ -0,0 +1,29 @@
+class Animal{ 
+    eat(){
+        return console.log('can eat...');
+    }
+}
+
+class Dog extends Animal{
+
+     bark(){
+        return console.log('can bark...')
+     }
+}
+
+class Cat extends Animal{
+    mew(){
+        return console.log('can mew');
+    }
+}
+
+
+const objDog = new Dog;
+objDog.eat();
+objDog.bark();
+
+const objCat = new Cat;
+objCat.eat();
+objCat.mew();
+
+
